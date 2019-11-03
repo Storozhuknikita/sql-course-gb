3a210f2256b81833f3ef086914bbaa5ef4f26cbf
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-11-03 16:30:15 +0300

    Finish 8 lesson and start course project

--- lesson-all.sql
+++ lesson-all.sql
@@ -109,6 +109,10 @@ from (
      ) as dummy
 group by gender;
 
+
+
+-- LESSON 8
+
 -- Сложные запросы с использованием JOIN
 -- ------------------------------------------------------------ Use vk
 use vk;
@@ -203,6 +207,7 @@ GROUP BY users.id
 ORDER BY total_likes DESC
 LIMIT 10;
 
+use vk;
 -- количество пользователей в сообществах
 select
     c.name,
@@ -211,3 +216,88 @@ from users u
          join users_communities uc on u.id = uc.user_id
          join communities c on c.id = uc.community_id
 group by c.id
+
+
+-- ---
+-- database SHOP
+-- Сложные запросы с использованием JOIN
+-- Транзакции, переменные, представления
+-- Lesson 8
+--
+
+-- ------------------------------------------------------------ Use shop
+use shop;
+
+-- CROSS JOIN
+SELECT users.id, users.name, users.birthday_at, orders.id, orders.user_id
+FROM users , orders;
+
+-- CROSS JOIN с условием
+SELECT users.id, users.name, users.birthday_at, orders.id, orders.user_id
+FROM users , orders
+WHERE users.id = orders.user_id;
+
+-- INNER JOIN (дает тот же результат)
+SELECT users.id, users.name, users.birthday_at, orders.id, orders.user_id
+FROM users
+         JOIN orders
+              ON users.id = orders.user_id;
+
+-- INNER JOIN с явным указанием типа
+SELECT users.id, users.name, users.birthday_at, orders.id, orders.user_id
+FROM users
+         INNER JOIN orders
+                    ON users.id = orders.user_id;
+
+-- INNER JOIN с агрегирующей функцией
+-- Подсчёт заказов пользователей
+SELECT users.name, COUNT(orders.user_id) AS total_orders
+FROM users
+         INNER JOIN orders
+                    ON users.id = orders.user_id
+GROUP BY orders.user_id
+ORDER BY total_orders;
+
+-- Аналогично запросу выше, но с сокращением записи имён таблиц
+SELECT u.name, COUNT(o.user_id) AS total_orders
+FROM users u
+         INNER JOIN orders o
+                    ON u.id = o.user_id
+GROUP BY o.user_id
+ORDER BY total_orders;
+
+-- LEFT OUTER JOIN (LEFT JOIN)
+SELECT users.id, users.name, users.birthday_at, orders.id, orders.user_id
+FROM users
+         LEFT OUTER JOIN orders
+                         ON users.id = orders.user_id;
+
+-- FULL OUTER JOIN (пример без содержательной нагрузки, просто для понимания синтаксиса)
+SELECT *
+FROM users
+         LEFT OUTER JOIN orders ON users.id = orders.user_id
+UNION
+SELECT *
+FROM users
+         RIGHT OUTER JOIN orders ON users.id = orders.user_id;
+
+-- Пользователи, у которых нет заказов
+SELECT users.id, users.name, users.birthday_at, orders.id, orders.user_id
+FROM users
+         LEFT OUTER JOIN orders
+                         ON users.id = orders.user_id
+WHERE orders.id IS NULL;
+/*
+-- RIGHT OUTER JOIN (RIGHT JOIN)
+SELECT users.id, users.name, users.birthday_at, orders.id, orders.user_id
+  FROM users
+    RIGHT OUTER JOIN orders
+  ON users.id = orders.user_id;
+*/
+-- Показано, что LEFT и RIGHT JOIN работают аналогично
+-- Снова пользователи без заказов
+SELECT users.id, users.name, users.birthday_at, orders.id, orders.user_id
+FROM orders
+         RIGHT OUTER JOIN users
+                          ON users.id = orders.user_id
+WHERE orders.id IS NULL;
